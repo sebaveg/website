117d74e5b45292c0064bcef3db0dff61ff2ea6a5
Sebastian Cardoso <sebastiancardoso92@gmail.com>
2019-09-26 20:01:31 -0300

    Optimizando imagenes

--- pages/_lang/projects.vue
+++ pages/_lang/projects.vue
@@ -1,35 +1,24 @@
 <template>
   <v-layout justify-center wrap row>
     <v-flex xs11 sm10 md9 lg7 xl6>
-      <h2 class="display-1 font-weight-bold my-5 text-center">Geounity</h2>
-      <p>
-        Geounity es una app moderadora de debates, investigadora de comunidades
-        y planificadora de acciones.
-      </p>
-      <p>
-        Con esta app el usuario que se registre en el sistema podrá crear o
-        contestar encuestas para su comunidad, ya sea, para su ciudad, para la
-        provincia, el país, el continente o para todo el mundo.
-      </p>
-      <p>
-        A su ves podrá abrir debates o entrar en alguno para dar su punto de vista sobre algún
-        problema presente en su comunidad. Al crear un debate o
-        dar su opinión podra apoyar su argumento con los datos de las encuestas.
-      </p>
-      <p>
-        Es todo un desafío desarrollar una app completa. Llenos de altos y bajos.
-        Es también muy divertido, emocionante y fuente de motivación poder
-        trabajar en un proyecto así. He tenido que diseñar y desarrollar toda la
-        base de datos con Postgres. Luego desarrollé una API con Node usando
-        Sequelize como ORM para comunicarme con la base de datos. Luego he
-        desarrollado el Frontend con Vue usando Nuxt en modo universal y por supuesto Vuex. También
-        aprendí a usar Gitlab y Github en el proceso. Y finalmente estoy
-        conociendo el Cloud de Google, la cual brinda muchísimas herramientas
-        para que nosotros los desarrolladores empecemos a servir nuestras aplicaciones. Herramientas
-        como App Engine para el frontend, Compute Engine para la API, Postgres
-        en Cloud y una VPN para que la API consulte la base de datos con una IP
-        privada. Es un continuo aprender, experimentar e implementar.
-      </p>
+      <v-container>
+        <v-row justify="space-around" align="start">
+          <v-col cols="12" md="5">
+            <h2 class="header mt-4">Reversi Game</h2>
+            <p>{{ $t('projects.reversi.description') }}</p>
+            <p><a href="https://reversi-game.sebastiancardoso92.now.sh">Reversi Game</a></p>
+            <p></p>
+            <v-img src="/reversi1.jpg" alt="Reversi-Game" style="margin:0 auto;max-width:320px" />
+          </v-col>
+          <v-col cols="12" md="5">
+            <h2 class="header mt-4">Geounity</h2>
+            <p>{{ $t('projects.geounity.description') }}</p>
+            <p><a href="https://geounity.org">Geounity.org</a></p>
+            <p></p>
+            <v-img src="/geounity1.png" alt="Reversi-Game" style="margin:0 auto;max-width:350px" />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-flex>
   </v-layout>
 </template>
