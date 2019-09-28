45bf721c95fa282eb4dc6aaff9ccf2587a3aa251
Sebastian Cardoso <sebastiancardoso92@gmail.com>
2019-09-28 15:57:27 -0300

    Compress images

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,7 +24,7 @@
     v-app-bar(dark fixed app)
       img(:src="logo" alt="Logo" height="40" width="40" class="mr-4")
       v-toolbar-title(v-text="title")
-      v-btn( nuxt :to="`/es` + $route.fullPath" exact text class="ml-5" @click="refresh") Español
+      v-btn( nuxt :to="`/es` + $route.fullPath" exact text class="ml-5") Español
       v-btn( nuxt :to="`/`" exact text ) English
       v-spacer
       v-app-bar-nav-icon(class="hidden-md-and-up" @click.stop="drawer = !drawer")
@@ -55,7 +55,7 @@ export default {
         {
           icon: "mdi-home",
           title: this.$t('links.home'),
-          to: this.localePath('')
+          to: this.localePath('/')
         },
         {
           icon: "mdi-laptop",
---
+++ pages/articles.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>
+    This page not exist yet.
+  </h1>
+</template>
